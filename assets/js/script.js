8aa81401ff4b1c7cdece971c937e69eb5aa8a1b5
Jay <11010206+resigned@users.noreply.github.com>
2019-08-23 19:48:06 -0400

    new songs

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ const startMusic = function () {
     analyser.connect(audioContext.destination)
 
     let i = 0
-    const playlist = ['Alan Walker - Fade.mp3', 'MachinimaSound.com_-_Dance_of_the_Pixies.mp3', 'Machinimasound.com_-_Remember_the_Dreams.mp3', 'Machinimasound.com_-_September_Sky.mp3', 'MachinimaSound.com_-_Shadows_of_the_Mind.mp3', 'Magnetic.Man.-.I.Need.Air.edit.mp3']
+    const playlist = ['Raiko - Lightning Child ft. Remi Willow [NCS Release].mp3', 'Syn Cole - Gizmo [NCS Release].mp3', 'Elek Tronomia - Elektronomia - Sky High.mp3', 'Alan Walker - Fade.mp3', 'MachinimaSound.com_-_Dance_of_the_Pixies.mp3', 'Machinimasound.com_-_Remember_the_Dreams.mp3', 'Machinimasound.com_-_September_Sky.mp3', 'MachinimaSound.com_-_Shadows_of_the_Mind.mp3', 'Magnetic.Man.-.I.Need.Air.edit.mp3']
 
     audio.addEventListener('ended', function () {
       i = ++i < playlist.length ? i : 0
